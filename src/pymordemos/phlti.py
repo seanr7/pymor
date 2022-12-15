e4e69fe62151d8febffc6e40f9e04a73f0fd4ed6
Jonas Nicodemus <jonas.nicodemus@icloud.com>
2022-12-15 11:33:17 +0100

    [pymordemos.phlti] test riccati equation

--- src/pymordemos/phlti.py
+++ src/pymordemos/phlti.py
@@ -4,6 +4,7 @@
 # License: BSD 2-Clause License (https://opensource.org/licenses/BSD-2-Clause)
 
 import numpy as np
+from slycot import sb02od
 from matplotlib import pyplot as plt
 from typer import run, Option
 
@@ -12,7 +13,7 @@ from pymor.reductors.bt import PRBTReductor
 from pymor.reductors.ph.ph_irka import PHIRKAReductor
 
 
-def msd(n=6, m_i=4, k_i=4, c_i=1, as_lti=False):
+def msd(n=6, m=2, m_i=4, k_i=4, c_i=1, as_lti=False):
     """Mass-spring-damper model as (port-Hamiltonian) linear time-invariant system.
 
     Taken from :cite:`GPBV12`.
@@ -58,15 +59,20 @@ def msd(n=6, m_i=4, k_i=4, c_i=1, as_lti=False):
         the ph |NumPy array| E, if `as_lti` is `False`.
     """
     n = int(n / 2)
-    m = 1
 
     A = np.array(
         [[0, 1 / m_i, 0, 0, 0, 0], [-k_i, -c_i / m_i, k_i, 0, 0, 0],
          [0, 0, 0, 1 / m_i, 0, 0], [k_i, 0, -2 * k_i, -c_i / m_i, k_i, 0],
          [0, 0, 0, 0, 0, 1 / m_i], [0, 0, k_i, 0, -2 * k_i, -c_i / m_i]])
 
-    B = np.array([[0, 1, 0, 0, 0, 0]]).T
-    C = np.array([[0, 1 / m_i, 0, 0, 0, 0]])
+    if m == 2:
+        B = np.array([[0, 1, 0, 0, 0, 0], [0, 0, 0, 1, 0, 0]]).T
+        C = np.array([[0, 1 / m_i, 0, 0, 0, 0], [0, 0, 0, 1 / m_i, 0, 0]])
+    elif m == 1:
+        B = np.array([[0, 1, 0, 0, 0, 0]]).T
+        C = np.array([[0, 1 / m_i, 0, 0, 0, 0]])
+    else:
+        assert False
 
     J_i = np.array([[0, 1], [-1, 0]])
     J = np.kron(np.eye(3), J_i)
@@ -113,12 +119,49 @@ def msd(n=6, m_i=4, k_i=4, c_i=1, as_lti=False):
 
     return J, R, G, P, S, N, E, Q
 
+def care(A, B=None, Q=None, R=None, S=None):
+    """Solve the continuous-time algebraic Riccati equation.
+            Q + A'X + XA - (S+XB)R^{-1}(S+XB)' = 0
+    """
+    dico = 'C'
+
+    if Q is None:
+        Q = np.zeros(A.shape)
+
+    n = A.shape[0]
+    m = B.shape[1]
+
+    # Q + A'X + XA + (-L-XB)R^{-1} (-L'-B'X) = 0
+    return sb02od(n, m, A, B, Q, R, dico, L=S)
+
+def kyp(A, B, C, D, X):
+    return np.block([[-A.T @ X - X @ A, C.T - X @ B], [C - B.T @ X, D + D.T]])
 
 def main(
         n: int = Option(100, help='Order of the Mass-Spring-Damper system.')
 ):
-    J, R, G, P, S, N, E, Q = msd(n)
+    # Riccati test
+    #################################################
+    A, B, C, D, E = msd(n=100, m=2, as_lti=True)
+    R = D + D.T + 1e-6 * np.eye(D.shape[0])
+    out = care(A, B, R=R, S=-C.T)
+    X = out[0]
 
+    # print(out[0])
+
+    W = kyp(A, B, C, D, X)
+
+    assert np.allclose(W, W.T)
+    w = np.linalg.eigvalsh(W)
+    # print(w)
+    # assert np.all(w > 0)
+
+    sol = A.T @ X + X @ A - (-C.T + X @ B) @ np.linalg.inv(R) @ (-C.T + X @ B).T
+    print(sol)
+    assert np.allclose(sol, np.zeros_like(sol))
+    #################################################
+
+    J, R, G, P, S, N, E, Q = msd(n, m=2)
     fom = PHLTIModel.from_matrices(J, R, G, Q=Q)
 
     prbt = PRBTReductor(fom)
